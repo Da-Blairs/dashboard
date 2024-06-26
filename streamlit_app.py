814959410f9f2a5281ce4be97f3b7332a7ec1b47
zoe <zoster@users.noreply.github.com>
2024-06-26 15:43:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -177,8 +177,8 @@ with col2:
             st.subheader(date)
             #print all the events on this date
             for event in events:
-                # pprint.pp(event['start'])
-                # if event['start'].get('date', event['start'].get('dateTime').split('T')[0]) == date:
+                pprint.pp(event['start'])
+                if event['start'].get('date') == date:
                 #     if event['start'].get('dateTime'):
                 #         start_datetime = datetime.datetime.fromisoformat(event['start']['dateTime'][:-6]) 
                 #         start_time = start_datetime.strftime('%I:%M %p').lower().lstrip('0')
