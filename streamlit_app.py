496178d9265b2c918ee787bc27bf5d4a24622216
zoe <zoster@users.noreply.github.com>
2024-06-26 16:48:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -176,7 +176,7 @@ with col2:
         
         for date in date_list:
             header = datetime.datetime.strptime(date, "%Y-%m-%d").strftime("%a %b %d")
-            markdown_output.append(f"### {header}")
+            markdown_output.append(f"<span class="date">{header}</span>")
             
             for event in events:
                 if 'date' in event['start']:
