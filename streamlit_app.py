f5570f9cc76bf814ef6eb5d7d3c4784e30d4dce2
zoe <zoster@users.noreply.github.com>
2024-06-26 15:59:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -177,17 +177,17 @@ with col2:
             st.subheader(date)
             #print all the events on this date
             for event in events:
-                if event['start'].get('date') == date:
-                    st.markdown(f"{event['title']} All Day")
-                if event['start']['dateTime'] and event['start']['dateTime'].split('T')[0] == date:
-                    start_datetime = datetime.datetime.fromisoformat(event['start']['dateTime'][:-6]) 
-                    pprint.pp(start_datetime)
+                if 'date' in event['start']:
+                    if event['start'].get('date') == date:
+                        st.markdown(f"{event['title']} All Day")
+                if 'dateTime' in event['start']:
+                    if event['start']['dateTime'].split('T')[0] == date:
+                        start_datetime = datetime.datetime.fromisoformat(event['start']['dateTime'][:-6]) 
+                        pprint.pp(start_datetime)
                         # start_time = start_datetime.strftime('%I:%M %p').lower().lstrip('0')
                 #         end_datetime = datetime.datetime.fromisoformat(event['end']['dateTime'][:-6]) 
                 #         end_time = end_datetime.strftime('%I:%M %p').lower().lstrip('0')
-                #         st.markdown(f"{event['title']} {start_time}-{end_time}")
-                else:
-                     pass   
+                #         st.markdown(f"{event['title']} {start_time}-{end_time}")  
     
     # Fetch events from Google Calendar
     calendar_events = get_google_calendar_events()
