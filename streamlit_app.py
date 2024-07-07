3a15e016021cb8709189304d448989885563d95e
Gavin Blair <gavinblair@users.noreply.github.com>
2024-07-07 19:22:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import csv
 import json
 import datetime
 import pytz
-import streamlit as st 
+import streamlit as st
 import requests
 import pprint
 import threading
@@ -19,9 +19,9 @@ from dotenv import load_dotenv
 from urllib.parse import urlparse, parse_qs
 from streamlit_calendar import calendar
 from io import BytesIO
- 
-st.set_page_config(page_title="Blair Dashboard", layout="wide") 
-  
+
+st.set_page_config(page_title="Blair Dashboard", layout="wide")
+
 with open( "app/style.css" ) as css:
     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
 
@@ -75,18 +75,18 @@ if hasattr(query_params, "code"):
     creds = flow.credentials
     # Store the new credentials in an environment variable
     os.environ['GOOGLE_CREDENTIALS_TOKEN'] = creds.to_json()
-    
+
     # Optionally, remove the query parameters from the URL to avoid repeated processing
     st.query_params.clear()
 
 def get_credentials():
     creds = None
-    
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
     # Check for existing credentials in environment variable
     token = os.getenv('GOOGLE_CREDENTIALS_TOKEN')
     if token:
         creds = Credentials.from_authorized_user_info(json.loads(token), SCOPES)
-    
+
     # Refresh or perform OAuth flow if credentials are invalid
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -96,7 +96,7 @@ def get_credentials():
         else:
             flow = Flow.from_client_config(credentials_info, SCOPES)
             flow.redirect_uri = redirect_uri
-            
+
             auth_url, _ = flow.authorization_url(access_type='offline', include_granted_scopes='true', prompt='consent')
             st.write(f"Please go to this URL for authorization: {auth_url}", auth_url)
 
@@ -105,16 +105,16 @@ def get_credentials():
 def midnight_toronto_iso():
     # Get the current UTC time
     now_utc = datetime.datetime.utcnow()
-    
+
     # Define the Toronto timezone
     toronto_tz = pytz.timezone('America/Toronto')
-    
+
     # Convert the current UTC time to Toronto time
     now_toronto = now_utc.astimezone(toronto_tz)
-    
+
     # Replace the time to midnight
     midnight_toronto = now_toronto.replace(hour=0, minute=0, second=0, microsecond=0)
-    
+
     # Convert to ISO format
     return midnight_toronto.isoformat()
 
@@ -122,9 +122,9 @@ def midnight_toronto_iso():
 def get_google_calendar_events():
     creds = get_credentials()
     service = build('calendar', 'v3', credentials=creds)
-    
+
     today = datetime.datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0).isoformat() + 'Z'
-        
+
     events_result = service.events().list(calendarId='primary', timeMin=midnight_toronto_iso(),
                                           maxResults=7, singleEvents=True,
                                           orderBy='startTime').execute()
@@ -173,17 +173,17 @@ event_icons = {
     "school": "<i class='fa-solid fa-pencil'></i>",
     "home": "<i class='fa-solid fa-suitcase'></i>",
     "camp": "<i class='fa-solid fa-campground'></i>",
- 
+
 }
-    
+
 def generate_events_markdown(events):
     date_list = get_event_dates(events)
     markdown_output = []
-    
+
     for date in date_list:
         header = datetime.datetime.strptime(date, "%Y-%m-%d")
         markdown_output.append(f"<span class='date'><span>{header.strftime('%a')}</span><span>{header.strftime('%b %d')}</span></span>")
-        
+
         for event in events:
             icon = ""
             event_title = event['title']
@@ -191,11 +191,11 @@ def generate_events_markdown(events):
                 if keyword.lower() in event_title.lower():
                     icon = event_icon
                     break  # Stop after finding the first matching keyword
-            
+
             if 'date' in event['start']:
                 if event['start'].get('date') == date:
                     markdown_output.append(f'<div class="event"><span class="time">All Day</span><br>{icon} {event_title}</div>')
-            
+
             if 'dateTime' in event['start']:
                 if event['start']['dateTime'].split('T')[0] == date:
                     start_datetime = datetime.datetime.fromisoformat(event['start']['dateTime'][:-6])
@@ -215,7 +215,7 @@ def update_weather():
     current = result_current["current_weather"]
     temp = current["temperature"]
     weathercode = current["weathercode"]
-    if weathercode == 0 or weathercode == 2: 
+    if weathercode == 0 or weathercode == 2:
         pprint.pp(result_current["daily"]["weather_code"])
         weathercode = result_current["daily"]["weather_code"][0]
     is_day = current["is_day"]
@@ -262,89 +262,100 @@ def gwen_read():
 
 def will_read():
     return who_read(name="will")
-    
-    
+
+
 
 # Streamlit setup
-col3, col1, col0, col2 = st.columns((1,1,1.5,2))
+col1, col0, col2 = st.columns((1,1,1.5))
 
 with col0:
     # Create a container to hold the image
     image_container = st.empty()
-    
+    html_content = '''
+    <style>
+        iframe {
+            border: none;
+            width: 100%;
+            height: 100%;
+            border-radius: 20px;
+        }
+    </style>
+    <iframe src="http://192.168.4.200:8081/frame.html" scrolling="no" frameborder="0" allowfullscreen></iframe>
+    '''
+    image_container.markdown(html_content, unsafe_allow_html=True)
     # Gwen Goals
     gwen_read = gwen_read()
     html_content = f'''
     <div id="gwen-goals">
         <i class="fa-solid fa-feather"></i>
         <em>Gwen Summer Goals</em>
-        <i class="fa-regular fa-circle-check"></i> <label for="icecream">Eat Icecream</label><br>
-        <i class="fa-regular fa-circle"></i> <label for="audiobooks">{gwen_read}/30 books read</label><br>
-        <i class="fa-regular fa-circle"></i> <label for="dnd">1/4 D&D Sessions</label><br>
+        <input type="checkbox" id="icecream"> <label for="icecream">Eat Icecream</label><br>
+        <input type="checkbox" id="audiobooks"> <label for="audiobooks">{gwen_read}/30 books read</label><br>
+        <input type="checkbox" id="dnd"> <label for="dnd">0/4 D&D Sessions</label><br>
     </div>
     '''
-    
+
     # Render the HTML content
     st.markdown(html_content, unsafe_allow_html=True)
- 
+
     # William Goals
     will_read = will_read() + 2
     html_content = f'''
     <div id="william-goals">
-        <i class="fa-solid fa-book-skull"></i>
+        <i class="fa-solid fa-skull-crossbones"></i>
         <em>Will Summer Goals</em>
-        <i class="fa-regular fa-circle-check"></i> <label for="icecream">Eat Icecream</label><br>
-        <i class="fa-regular fa-circle"></i> <label for="audiobooks">{will_read}/5 Ascendant Series</label><br>
-        <i class="fa-regular fa-circle"></i> <label for="dnd">1/4 D&D Sessions</label><br>
+        <input type="checkbox" id="icecream"> <label for="icecream">Eat Icecream</label><br>
+        <input type="checkbox" id="audiobooks"> <label for="audiobooks">{will_read}/5 Ascendant Series</label><br>
+        <input type="checkbox" id="dnd"> <label for="dnd">0/4 D&D Sessions</label><br>
     </div>
     '''
-    
+
     # Render the HTML content
     st.markdown(html_content, unsafe_allow_html=True)
-    
+
     # Sadie Goals
     html_content = '''
     <div id="sadie-goals">
         <i class="fa-solid fa-heart"></i>
         <em>Sadie Summer Goals</em>
-        <i class="fa-regular fa-circle-check"></i> <label for="icecream">Learn to Swim</label><br>
-        <i class="fa-regular fa-circle"></i> <label for="audiobooks">Learn to Read</label><br>
-        <i class="fa-regular fa-circle-check"></i> <label for="dnd">Play D&D</label><br>
+        <input type="checkbox" id="icecream"> <label for="icecream">Learn to Swim</label><br>
+        <input type="checkbox" id="audiobooks"> <label for="audiobooks">Learn to Read</label><br>
+        <input type="checkbox" id="dnd"> <label for="dnd">Play D&D</label><br>
     </div>
     '''
-    
+
     # Render the HTML content
     st.markdown(html_content, unsafe_allow_html=True)
-    
-    
+
+
 
 with col1:
 
     weather = st.markdown(f'<div id="weather"></div>' , unsafe_allow_html= True)
-    try: 
+    try:
       update_weather()
     except:
       pass
 
     books_read = books_read()
     if(books_read):
-        st.markdown(f'<div id="steps"><span><span class="count">{books_read}</span>&nbsp;summer reads</span><span></span><i class="fa-solid fa-book"></i></div>' , unsafe_allow_html= True)
-    
-    st.markdown(f'<div id="swims"><span><span class="count">6</span>&nbsp;swim days</span><span></span><i class="fa-solid fa-person-swimming"></i></div>' , unsafe_allow_html= True)
+        st.markdown(f'<div id="steps"><span class="count">{books_read}</span><span>summer reads</span></div>' , unsafe_allow_html= True)
+
+    #st.markdown(f'<div id="swims"><span class="count">0</span><span>hours swimming</span></div>' , unsafe_allow_html= True)
 
     dinner = st.markdown(f'<div id="food"><i class="fa-solid fa-utensils"></i><p><span class="count">Dinner Today</span><br><span>No plans</span></p><p><span class="count">Dinner Tomorrow</span><br><span>No plans</span></p></div>' , unsafe_allow_html= True)
-    
-    # Create a placeholder for the clock
-    clock_placeholder = st.empty() 
 
-    
-with col2:     
+    # Create a placeholder for the clock
+    clock_placeholder = st.empty()
+
+
+with col2:
     # Get credentials
     creds = get_credentials()
     if not creds:
         st.error("Failed to obtain credentials.")
         st.stop()
-    
+
     # Fetch events from Google Calendar
     calendar_events = []
     try:
@@ -352,7 +363,7 @@ with col2:
     except:
       pass
     # pprint.pp(calendar_events)
-    
+
     if calendar_events:
         calendar_markdown = generate_events_markdown(calendar_events)
         st.markdown(f'<div class="event-list">{calendar_markdown}</div>', unsafe_allow_html=True)
@@ -362,7 +373,7 @@ with col2:
 # Define the timezone for Toronto
 toronto_tz = pytz.timezone('America/Toronto')
 
-def updateDinner():    
+def updateDinner():
     creds = get_credentials()
     service = build('calendar', 'v3', credentials=creds)
     events_result = service.events().list(calendarId='d784fd49eb9057cf7c36c7d91c36b40613c3a86da4e351020ae0d1eaa4568cc2@group.calendar.google.com',
@@ -378,17 +389,9 @@ def updateDinner():
     dinner_tomorrow = dinners[1] if len(dinners) > 1 else "No plans"
 
     dinner.markdown(f'<div id="food"><i class="fa-solid fa-utensils"></i><p><span class="count">Dinner Today</span><br><span>{dinner_today}</span></p><p><span class="count">Dinner Tomorrow</span><br><span>{dinner_tomorrow}</span></p></div>' , unsafe_allow_html= True)
-    
+
 updateDinner()
 
-# Function to update the image
-def updateImage():
-    # Get the image from the URL
-    response = requests.get(f'http://generationgeneration.ca/frame.jpg')
-    image = Image.open(BytesIO(response.content))
-    # Display the image in the container
-    image_container.image(image, use_column_width=True)
- 
 def updateClock(use_colon=True):
     # Get the current time
     current_time = datetime.datetime.now(toronto_tz).strftime('%I:%M %p' if use_colon else '%I %M %p')
@@ -406,28 +409,21 @@ def run_schedule():
 
         try:
             updateClock(current_second % 2)
-        except: 
+        except:
             pass
 
-        if current_second % 2 == 0:
-            try:
-                updateImage()
-            except: 
-                pass
-
         if current_minute % 5 == 0:
-            try: 
+            try:
                 update_weather()
-            except: 
+            except:
                 pass
             try:
                 updateDinner()
-            except: 
-                pass    
-        
+            except:
+                pass
+
         # Wait for 1 second before updating the time again
         sleep(1)
 
 updateImage()
 run_schedule()
-
