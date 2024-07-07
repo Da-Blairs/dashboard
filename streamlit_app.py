73a0d5c1dd7845d0d73071e353519d5d18072c32
zoe <zoster@users.noreply.github.com>
2024-07-07 18:56:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -266,7 +266,7 @@ def will_read():
     
 
 # Streamlit setup
-col3, col1, col0, col2 = st.columns((1,1,1,1.5))
+col3, col1, col0, col2 = st.columns((1,1,1.5,2))
 
 with col0:
     # Create a container to hold the image
