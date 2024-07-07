a4bec66d857563861eee0233ecfe02929bccf38b
zoe <zoster@users.noreply.github.com>
2024-07-07 18:52:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -278,7 +278,7 @@ with col0:
     <div id="gwen-goals">
         <i class="fa-solid fa-feather"></i>
         <em>Gwen Summer Goals</em>
-        <input type="checkbox" id="icecream"> <label for="icecream">Eat Icecream</label><br>
+        <input type="checkbox" checked="checked" id="icecream"> <label for="icecream">Eat Icecream</label><br>
         <input type="checkbox" id="audiobooks"> <label for="audiobooks">{gwen_read}/30 books read</label><br>
         <input type="checkbox" id="dnd"> <label for="dnd">0/4 D&D Sessions</label><br>
     </div>
@@ -293,7 +293,7 @@ with col0:
     <div id="william-goals">
         <i class="fa-solid fa-book-skull"></i>
         <em>Will Summer Goals</em>
-        <input type="checkbox" id="icecream"> <label for="icecream">Eat Icecream</label><br>
+        <input type="checkbox" checked="checked" id="icecream"> <label for="icecream">Eat Icecream</label><br>
         <input type="checkbox" id="audiobooks"> <label for="audiobooks">{will_read}/5 Ascendant Series</label><br>
         <input type="checkbox" id="dnd"> <label for="dnd">0/4 D&D Sessions</label><br>
     </div>
@@ -307,7 +307,7 @@ with col0:
     <div id="sadie-goals">
         <i class="fa-solid fa-heart"></i>
         <em>Sadie Summer Goals</em>
-        <input type="checkbox" id="icecream"> <label for="icecream">Learn to Swim</label><br>
+        <input type="checkbox" checked="checked" id="icecream"> <label for="icecream">Learn to Swim</label><br>
         <input type="checkbox" id="audiobooks"> <label for="audiobooks">Learn to Read</label><br>
         <input type="checkbox" id="dnd"> <label for="dnd">Play D&D</label><br>
     </div>
