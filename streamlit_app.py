3edc7fdc38e862536da4c52eb453847f828afbcb
Gavin Blair <gavinblair@users.noreply.github.com>
2024-07-07 22:04:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -348,7 +348,7 @@ with col1:
 
     books_read = books_read()
     if(books_read):
-        st.markdown(f'<div id="steps"><span><span class="count">{books_read}</span>&nbsp;summer reads</span><span></span><i class="fa-solid fa-book"></i></div>' , unsafe_allow_html= True)
+        st.markdown(f'<div id="steps"><span><span class="count">{books_read}</span>&nbsp;summer reads</span><span class="count">&nbsp;</span><i class="fa-solid fa-book"></i></div>' , unsafe_allow_html= True)
     
     st.markdown(f'<div id="swims"><span><span class="count">6</span>&nbsp;swim days</span><span></span><i class="fa-solid fa-person-swimming"></i></div>' , unsafe_allow_html= True)
 
