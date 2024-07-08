d0d85fde4741436ef5e0d64306361df644b36ba2
zoe <zoster@users.noreply.github.com>
2024-07-08 15:12:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -219,6 +219,37 @@ def generate_events_markdown(events):
 
     return "\n".join(markdown_output)
 
+weather_icons = {
+    "clearsky_day" : "wi-day-sunny",
+    "clearsky_night" : "wi-night-clear",
+    "fair_day" : "wi-day-sunny-overcast",
+    "fair_night" : "wi-night-alt-partly-cloudy",
+    "partlycloudy_day" : "wi-day-cloudy",
+    "partlycloudy_night" : "wi-night-alt-cloudy",
+    "cloudy" : "wi-cloudy",
+    "lightrainshowers_day" : "wi-day-showers",
+    "lightrainshowers_night" : "wi-night-alt-showers",
+    "rainshowers_day" : "wi-day-rain",
+    "rainshowers_night" : "wi-night-alt-rain",
+    "heavyrainshowers_day" : "wi-day-rain-wind",
+    "heavyrainshowers_night" : "wi-night-alt-rain-wind",
+    "lightsnowshowers_day" : "wi-day-snow",
+    "lightsnowshowers_night" : "wi-night-alt-snow",
+    "snowshowers_day" : "wi-day-snow-wind",
+    "snowshowers_night" : "wi-night-alt-snow-wind",
+    "heavysnowshowers_day" : "wi-day-snow-thunderstorm",
+    "heavysnowshowers_night" : "wi-night-alt-snow-thunderstorm",
+    "lightrain" : "wi-rain-mix",
+    "rain" : "wi-rain",
+    "heavyrain" : "wi-rain-wind",
+    "lightsnow" : "wi-snow",
+    "snow" : "wi-snow",
+    "heavysnow" : "wi-snow-wind",
+    "sleet" : "wi-sleet",
+    "thunderstorm" : "wi-thunderstorm",
+    "fog" : "wi-fog",
+}
+
 def update_weather2():
     lat = "42.9836"
     lng = "-81.2497"
@@ -233,19 +264,23 @@ def update_weather2():
         if response_current.status_code == 200:
             result_current = response_current.json()
             temp = result_current["properties"]["timeseries"][0]["data"]["instant"]["details"]["air_temperature"]
-            weathercode = result_current["properties"]["timeseries"][0]["data"]["next_1_hours"]["summary"]["symbol_code"]
+            weathersymbol = result_current["properties"]["timeseries"][0]["data"]["next_1_hours"]["summary"]["symbol_code"]
+            if weathersymbol in weather_icons:
+                weathercode = weather_icons[weathersymbol]
+            else:
+                weathercode = "wi-alien"
 
             # Update cache
             weather_cache["data"] = (temp, weathercode)
             weather_cache["expires"] = datetime.datetime.utcnow() + datetime.timedelta(hours=1)  # assuming data is valid for 1 hour
             
-            st.markdown(f'<div id="weather">{temp}°C<i class="big-icon wi wi-wmo4680-{weathercode}"></i></div>', unsafe_allow_html=True)
+            st.markdown(f'<div id="weather">{temp}°C<i class="big-icon wi {weathercode}"></i></div>', unsafe_allow_html=True)
         else:
             st.error("Failed to fetch weather data")
     else:
         # Use cached data
         temp, weathercode = weather_cache["data"]
-        st.markdown(f'<div id="weather">{temp}°C<i class="big-icon wi wi-wmo4680-{weathercode}"></i></div>', unsafe_allow_html=True)
+        st.markdown(f'<div id="weather">{temp}°C<i class="big-icon wi {weathercode}"></i></div>', unsafe_allow_html=True)
 
 # Call the function to update weather
 update_weather2()
