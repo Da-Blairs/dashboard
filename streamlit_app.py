0b8d4ed62fcb438e4757217f2bcaa48fe044efd1
zoe <zoster@users.noreply.github.com>
2024-07-08 21:49:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -433,7 +433,7 @@ def generate_donut_chart_svg(values, colors, labels, inner_radius=20):
 def render_svg(svg):
     """Renders the given svg string."""
     b64 = base64.b64encode(svg.encode('utf-8')).decode("utf-8")
-    html = r'<img src="data:image/svg+xml;base64,%s"/>' % b64
+    html = r'<div id="{div_id}"><img src="data:image/svg+xml;base64,%s"/></div>' % b64
     st.write(html, unsafe_allow_html=True)
 
 def render_donut_chart(values, colors, labels):
