c1a450844c6244290864ae968ce84efaddd4e1d5
zoe <zoster@users.noreply.github.com>
2024-07-08 21:33:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ import requests
 import pprint
 import threading
 import gspread
-from math import cos, radians
+from math import cos, radians, sin
 from collections import Counter
 import pandas as pd
 import plotly.express as px
