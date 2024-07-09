0136f13b2120a9786ff720cdc7374119325556d9
zoe <zoster@users.noreply.github.com>
2024-07-09 08:33:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -148,7 +148,7 @@ def get_google_calendar_events():
     
     today = datetime.datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0).isoformat() + 'Z'
         
-    events_result = service.events().list(calendarId='family13503006410585813018@group.calendar.google.com', 
+    events_result = service.events().list(calendarId='e47c4628e6961718557b31f77a397c46c53367a2b87ca027ceb7b1aff272b546@group.calendar.google.com',
                                           timeMin=midnight_toronto_iso(),
                                           maxResults=10, singleEvents=True,
                                           orderBy='startTime').execute()
