76e293c62004c5a126d46f17c529ce3a5d6b6e68
zoe <zoster@users.noreply.github.com>
2024-07-09 20:13:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -406,8 +406,8 @@ def generate_donut_chart_svg_from_counter(counter):
         '#92d050',  # Light Green
         '#ffc000',  # Gold
         '#b6d7a8',  # Pale Green
-        '#ffccff',  # Light Pink
         '#ffeb99',  # Light Yellow
+        '#ffccff',  # Light Pink
         '#cfe2f3',  # Light Blue
         '#ff9999',  # Light Salmon
         '#ffcc99',  # Peach
