e6193294c72636d3610aa22e083dec1e67d487bf
Zoe Blair <zoe.blair@rtraction.com>
2024-07-11 17:35:56 -0400

    moving weather to it's own file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import base64
+from weather import weather_forecast
 from flask import Flask, request, redirect, session, render_template, jsonify
 import requests
 import datetime
@@ -8,7 +9,7 @@ from google.auth.transport.requests import Request
 import json
 import os
 import pytz
-from summer_reads import summer_reads_total, summer_reads_svg
+from summer_reads import summer_reads_total, summer_reads_svg, gwen_read, will_read
 from summer_swims import summer_swims_total, summer_swims_svg
 from gcal import google_authorize, google_callback, gcal_dinner, gcal_work, gcal_events
 
@@ -35,81 +36,13 @@ def home():
         styles_css=styles_css,
         weather_icons_css=weather_icons_css,
         current_date=current_date,
-        gwen_read=6,
-        will_read=3
+        gwen_read=gwen_read(),
+        will_read=will_read()
     )
 
 @app.route('/weather')
 def weather():
-    USER_AGENT = "blairs.streamlit.app/1.0 (https://blairs.streamlit.app/contact)"
-
-    weather_icons = {
-        "clearsky_day" : "wi-day-sunny",
-        "clearsky_night" : "wi-night-clear",
-        "fair_day" : "wi-day-sunny-overcast",
-        "fair_night" : "wi-night-alt-partly-cloudy",
-        "partlycloudy_day" : "wi-day-cloudy",
-        "partlycloudy_night" : "wi-night-alt-cloudy",
-        "cloudy" : "wi-cloudy",
-        "lightrainshowers_day" : "wi-day-showers",
-        "lightrainshowers_night" : "wi-night-alt-showers",
-        "rainshowers_day" : "wi-day-rain",
-        "rainshowers_night" : "wi-night-alt-rain",
-        "heavyrainshowers_day" : "wi-day-rain-wind",
-        "heavyrainshowers_night" : "wi-night-alt-rain-wind",
-        "lightsnowshowers_day" : "wi-day-snow",
-        "lightsnowshowers_night" : "wi-night-alt-snow",
-        "snowshowers_day" : "wi-day-snow-wind",
-        "snowshowers_night" : "wi-night-alt-snow-wind",
-        "heavysnowshowers_day" : "wi-day-snow-thunderstorm",
-        "heavysnowshowers_night" : "wi-night-alt-snow-thunderstorm",
-        "lightrain" : "wi-rain-mix",
-        "rain" : "wi-rain",
-        "heavyrain" : "wi-rain-wind",
-        "lightsnow" : "wi-snow",
-        "snow" : "wi-snow",
-        "heavysnow" : "wi-snow-wind",
-        "sleet" : "wi-sleet",
-        "thunderstorm" : "wi-thunderstorm",
-        "fog" : "wi-fog",
-    }
-
-    # Global cache for storing weather data and expiry timestamp
-    weather_cache = {
-        "expires": datetime.datetime.utcnow() - datetime.timedelta(seconds=1),  # already expired
-        "data": None
-    }
-    # Check if cache is expired
-    if datetime.datetime.utcnow() > weather_cache["expires"]:
-        lat = "42.9836"
-        lng = "-81.2497"
-        headers = {"User-Agent": USER_AGENT}
-        response_current = requests.get(
-            f'https://api.met.no/weatherapi/locationforecast/2.0/complete?lat={lat}&lon={lng}',
-            headers=headers
-        )
-        
-        if response_current.status_code == 200:
-            result_current = response_current.json()
-            temp = round(result_current["properties"]["timeseries"][0]["data"]["instant"]["details"]["air_temperature"])
-            uv = round(result_current["properties"]["timeseries"][0]["data"]["instant"]["details"]["ultraviolet_index_clear_sky"])
-            weathersymbol = result_current["properties"]["timeseries"][0]["data"]["next_1_hours"]["summary"]["symbol_code"]
-            weathercode = weather_icons.get(weathersymbol, "wi-alien")
-
-            # Update cache
-            weather_cache["data"] = (temp, uv, weathercode)
-            weather_cache["expires"] = datetime.datetime.utcnow() + datetime.timedelta(hours=1)  # assuming data is valid for 1 hour
-        else:
-            return jsonify({"error": "Failed to fetch weather data"}), 500
-    else:
-        # Use cached data
-        temp, uv, weathercode = weather_cache["data"]
-
-    return jsonify({
-        "temperature": f"{temp}°C",
-        "uv": f"UV {uv}",
-        "weathercode": weathercode
-    })
+    return weather_forecast()
 
 @app.route('/dinner')
 def dinner():
--- summer_reads.py
+++ summer_reads.py
@@ -4,6 +4,31 @@ from collections import Counter
 from math import cos, sin, radians
 from pie_chart_svg import pie_chart_svg
 
+def who_read(name):
+    url="https://docs.google.com/spreadsheets/d/e/2PACX-1vRTRhgd6hpw5XvVvS-dRtPPcQQTVigYRk7zzKCXiEtrW-LbwJn9qI8LEa8RFnz5mNd95h8Zb_bjWkaJ/pub?gid=0&single=true&output=csv"
+    # Fetch the CSV data from the URL
+    response = requests.get(url)
+
+    # Check if request was successful
+    if response.status_code == 200:
+        # Decode the content to text and split it into lines
+        lines = response.content.decode('utf-8').splitlines()
+
+        # Use csv.reader to read the lines
+        csv_reader = csv.reader(lines)
+
+        # Count rows where the first column starts with name
+        name = name.strip().lower()
+        return sum(1 for row in csv_reader if row and row[0].strip().lower() == name)
+    else:
+        return False
+
+def gwen_read():
+    return who_read(name="gwen")
+
+def will_read():
+    return who_read(name="will")
+
 def reader_count():
     url="https://docs.google.com/spreadsheets/d/e/2PACX-1vRTRhgd6hpw5XvVvS-dRtPPcQQTVigYRk7zzKCXiEtrW-LbwJn9qI8LEa8RFnz5mNd95h8Zb_bjWkaJ/pub?gid=0&single=true&output=csv"
 
